1fbc109370929336e02a999cd8c670a104cf1901
dmartinezsaa <dani@dani-GE60-2QD>
2017-11-23 13:49:49 +0100

    edición1

--- src/boletin11/Boletin11.java
+++ src/boletin11/Boletin11.java
@@ -15,7 +15,7 @@ public class Boletin11{
      * @param args the command line arguments
      */
     public static void main(String[] args){
-        Seleccion Isco=new Seleccion();
+        Seleccion Isco=new Jugador(22,"centrocampista",189,25,"Francisco Román","Alarcón");
         Jugador Morata=new Jugador(9, "delantero",123,25,"Álvaro","Morata");
         Entrenador Lopetegui=new Entrenador("A123",155,44,"Julen","Lopetegui");
         Masajista Pedro=new Masajista("Grado en fisioterapia",7,111,39,"Pedro","González");
